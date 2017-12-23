eeb989f6da7bb5a42f3af2470b2ec13649480c1d
Brandon David <brandonhudavid@berkeley.edu>
2017-12-23 00:22:21 -0800

    scrollbar feature; jpeg and png supported

--- ImageColorSort_gui.py
+++ ImageColorSort_gui.py
@@ -24,7 +24,7 @@ def color_in_img(img, color=None):
 	num_pixels = len(pixel_data)
 	for pixel in pixel_data:
 		# most time occupied when converting to HSV
-		hsv = colorsys.rgb_to_hsv(*pixel)
+		hsv = colorsys.rgb_to_hsv(pixel[0], pixel[1], pixel[2])
 		# add weight values to HSV keys in dictionary
 		try:
 			color_dict[hsv] += 1
@@ -81,13 +81,15 @@ def color_in_img(img, color=None):
 colorname = ["red", "orange", "yellow", "green", "cyan", "blue", \
 				"purple", "pink"]
 
-
+### GUI SETUP ###
 class GUI:
 
 	def __init__(self, master, color):
-		# window size
+		# title, icon, window size, default font
 		master.title("ImageColorSort")
+		master.iconbitmap('bd_icon.ico')
 		master.geometry("475x700")
+		master.option_add("*Font", "Helvetica 14")
 
 		# pack frames
 		self.topFrame = Frame(master)
@@ -101,27 +103,34 @@ class GUI:
 		self.bottomFrame = Frame(master)
 		self.bottomFrame.pack()
 
-		# bg image
+		# bg image created in canvas underneath output frame
 		self.bg_img = PhotoImage(file="bd_bg.gif")
 		self.bg_canvas = Canvas(master)
-		self.bg_canvas.pack(side=TOP, fill=BOTH, expand=YES)
+		self.bg_canvas.pack(side=LEFT, fill=BOTH, expand=YES)
 		self.bg_canvas.create_image(0, 0, image=self.bg_img, anchor='nw')
-
 		self.outputFrame = Frame(self.bg_canvas)
 		self.outputFrame.pack()
-		# creates and destroys text in mode 3
-		self.helperFrame = Frame(self.outputFrame)
-		self.helperFrame.pack(side=TOP)
 
-		# change default font
-		master.option_add("*Font", "Helvetica 14")
-		# title
+		# helper frame in canvas, stores and destroys text in color sort
+		self.helperFrame = Frame(self.bg_canvas)
+		# vertical scrollbar for mode 3
+		self.vsb = Scrollbar(self.bg_canvas, orient="vertical", command=self.bg_canvas.yview)
+		# self.bg_canvas.configure(yscrollcommand=self.vsb.set)
+		# self.vsb.pack(side=RIGHT, fill=Y)
+		# self.bg_canvas.create_window((237,0), window=self.helperFrame, anchor=N,\
+		# 	tags="self.helperFrame")
+		#self.helperFrame.bind("<Configure>", self.onFrameConfigure)
+
+		# header
 		self.header = Label(self.topFrame, text="ImageColorSort", \
 			font="Helvetica 24 bold").pack(pady=(10,0))
 		self.developer = Label(self.topFrame, text="developed by " \
 			+ "Brandon David\n" + \
 			"(https://github.com/brandonhudavid/ImageColorSort)", \
-			font="Helvetica 13").pack(pady=(0,10))
+			font="Helvetica 13").pack()
+		self.supported = Label(self.topFrame, text="Supported file types: "\
+			+".jpg, .jpeg, .png", font="Helvetica 14 underline").pack(pady=10)
+
 		# Buttons for modes
 		self.button_comp = Radiobutton(self.topFrame, text="Find the most " \
 			+ "used color in an image", value=1, variable="mode", \
@@ -135,13 +144,14 @@ class GUI:
 			+ "images in a directory by color", value=3, \
 			variable="mode", command=lambda: self.modeswitch(2))
 		self.button_sort.pack()
+
 		# Buttons to select file
 		self.select = Button(self.fileFrame, text="Select file...", \
 			command=self.fileselect).pack()
 		self.file = Label(self.fileFrame, text="no file selected")
 		self.file.pack()
 
-		# creating button for each color
+		# Button for each color
 		self.red = Radiobutton(self.midFrame, text="red", \
 			state=NORMAL, value=1, variable="colors", command=lambda: \
 			self.colorselect("red"))
@@ -174,6 +184,7 @@ class GUI:
 		self.initialize = Button(self.bottomFrame, text="Initialize", \
 			state=DISABLED, command=lambda: self.mode(self.modefunc))
 		self.initialize.pack()
+
 		# Output text
 		self.output = Label(self.outputFrame)
 		self.output.pack(side=BOTTOM)
@@ -181,33 +192,43 @@ class GUI:
 		# initial selection of buttons
 		self.red.invoke()
 		self.button_comp.invoke()
-		self.modefunc = "color_choose"
+		self.modefunc = "color_comp"
 		mainloop()
 
+	# opens image or directory, depending on mode
 	def fileselect(self):
 		if self.modefunc == "color_sort":
 			self.filename = filedialog.askdirectory()
 			if self.filename != "":
 				self.dirpath = self.filename
 				self.file["text"] = "/"+os.path.split(self.filename)[1]
+				if len(self.file["text"]) > 30:
+					self.file["text"] = self.file["text"][:20]+"..."\
+						+self.file["text"][-14:]
 		else:
+			# supports .jpg, .png, and .jpeg image formats
 			self.filename = filedialog.askopenfilename(\
-				filetypes=[("Image File",'.jpg')])
+				filetypes=[("Image File",'.jpg .png .jpeg')])
 			if self.filename != "":
 				self.img = Image.open(self.filename, 'r')
 				self.file["text"] = os.path.split(self.filename)[1]
-
+				if len(self.file["text"]) > 30:
+					self.file["text"] = self.file["text"][:15]+"..."\
+						+self.file["text"][-14:]
 		self.initswitch()
 
+	# enable "Initialize" button if file is selected
 	def initswitch(self):
 		if self.file["text"] != "no file selected":
 			self.initialize.config(state=NORMAL)
 		else:
 			self.initialize.config(state=DISABLED)
 
+	# used by color buttons to change self.color
 	def colorselect(self, color):
 		self.color = color
 
+	# switching modes; reconfigure buttons, functions, file selection
 	def modeswitch(self, switch):
 		if switch:
 			for button in (self.red, self.orange, self.yellow, self.green, \
@@ -225,10 +246,21 @@ class GUI:
 		self.file["text"] = "no file selected"
 		self.initswitch()
 
+	# run when "Initialize" is clicked
 	def mode(self, func):
-		self.helperFrame.destroy()
-		self.helperFrame = Frame(self.outputFrame)
-		self.helperFrame.pack()
+		# canvas, helperFrame, & scrollbar management
+		self.bg_canvas.delete("all")
+		self.vsb.destroy()
+		self.bg_canvas.yview_moveto(0.0)
+		self.bg_canvas.create_image(0, 0, image=self.bg_img, anchor='nw')
+		self.helperFrame = Frame(self.bg_canvas)
+		self.outputFrame = Frame(self.bg_canvas)
+		self.outputFrame.pack(side=BOTTOM)
+		self.vsb = Scrollbar(self.bg_canvas, orient="vertical")
+		self.bg_canvas.create_window((237,0), window=self.helperFrame, anchor=N,\
+			tags="self.helperFrame")
+
+		# different functions for different modes
 		if func == "color_comp":
 			self.color_comp(self.img)
 		if func == "color_choose":
@@ -237,51 +269,67 @@ class GUI:
 			self.output["text"] = ""
 			self.color_sort(self.dirpath, self.color)
 
+	# used by scrollbar to scroll through helperFrame
+	def onFrameConfigure(self, event):
+		'''Reset the scroll region to encompass the inner frame'''
+		self.bg_canvas.configure(scrollregion=self.bg_canvas.bbox("all"))
+
+	# mode 1 (color_value returns string if Error)
 	def color_comp(self, img):
 		color_value = color_in_img(img)
 		if isinstance(color_value, str):
 			self.output["text"] = color_value
 		else:
-			comp_output = color_value[0] + " is the most used color, " + \
+			comp_output = color_value[0] + " is the most used color,\n" + \
 			"composing " + str(percentage(color_value[1])) + "% of the image"
 			self.output["text"] = comp_output
 
+	# mode 2
 	def color_choose(self, img, color):
 		color_value = color_in_img(img, color)
-		if isinstance(color_value, str):
-			self.output["text"] = color_value
-		else:
-			choose_output = "color selected: " + color + "\n" \
-			+ "percent composition: " + str(percentage(color_value)) + "%"
-			self.output["text"] = choose_output
+		choose_output = "color selected: " + color + "\n" \
+		+ "percent composition: " + str(percentage(color_value)) + "%"
+		self.output["text"] = choose_output
 
+	# mode 3
 	def color_sort(self, path, color):
 		direc = os.listdir(path)
 		files = {}
+		# only opens .jpg, .png, .jpeg
 		for infile in direc:
-			if infile[-4:] == ".jpg" or infile[-4:] == ".JPG":
+			if infile[-4:] == ".jpg" or infile[-4:] == ".png" or\
+				infile[-5:] == ".jpeg" or infile[-4:] == ".JPG" or\
+				infile[-4:] == ".PNG" or infile[-5:] == ".JPEG":
 				img = Image.open(path+"/"+infile)
 				decimal = color_in_img(img, color)
 				files[infile] = decimal
 		files_sorted = sorted(files.items(), key=operator.itemgetter(1), \
 		 				reverse=True)
+		# return Error if no supported files
 		if len(files_sorted) == 0:
-			Label(self.helperFrame, text="Error: no jpg file in selected "\
-				+"directory").pack()
+			self.output["text"] = "Error: no supported image file in "\
+				+"selected directory\n(must be .jpg, .jpeg, or .png)"
 		else:
+			# if more than 15 files, add scrollbar
+			if len(files_sorted) > 15:
+				self.vsb.configure(command=self.bg_canvas.yview)
+				self.bg_canvas.configure(yscrollcommand=self.vsb.set)
+				self.vsb.pack(side=RIGHT, fill=Y)
+				self.helperFrame.bind("<Configure>", self.onFrameConfigure)
+			# create two columns for file name and color composition
 			Label(self.helperFrame, text="file name",\
 				font="Helvetica 14 underline").grid(row=0)
 			Label(self.helperFrame, text="composition of "+color,\
 				font="Helvetica 14 underline").grid(row=0, column=1)
 			i = 1
 			for value in files_sorted:
-				if len(str(value[0])) > 19:
-					Label(self.helperFrame, text=str(value[0])[:15]+str("...")+\
-					str(value[0])[-4:]).grid(row=i, sticky=E)
+				if len(str(value[0])) > 30:
+					Label(self.helperFrame, text=str(value[0])[:15]\
+					+"..."+str(value[0])[-10:]).grid(row=i, sticky=E)
 				else:
 					Label(self.helperFrame, text=value[0]).grid(row=i)
-				Label(self.helperFrame, text=str(percentage(value[1]))[:8]+"%").\
-					grid(row=i, column=1)
+				Label(self.helperFrame, text=str(percentage(value[1]))[:8]\
+				+"%").grid(row=i, column=1)
 				i+=1
 
 root = Tk()
